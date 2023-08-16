736dbea77334ea0fd90a09b942d50ec276cc4427
CaduhPavei <CaduhPavei@Gmail.com>
2023-08-15 21:59:05 -0300

    Exercício feito em sala de aula.

---
+++ src/Aula4/Exercicio3_1.java
@@ -0,0 +1,18 @@
+package Aula4;
+import javax.swing.*;
+
+public class Exercicio3_1 {
+    public static void main(String[] args) {
+        Integer inteiro = Integer.parseInt(JOptionPane.
+                showInputDialog(null, "Digite um inteiro:", "Entrada", JOptionPane.QUESTION_MESSAGE));
+//        System.out.println(inteiro);
+        Integer inteiro2 = Integer.parseInt(JOptionPane.
+                showInputDialog(null, "Digite um inteiro:", "Entrada", JOptionPane.QUESTION_MESSAGE));
+//        System.out.println(inteiro2);
+        int soma=inteiro2+inteiro;
+        JOptionPane.
+                showMessageDialog(null,"O resultado da soma é: "+soma,"Calculo",JOptionPane.INFORMATION_MESSAGE);
+
+
+    }
+}
--- src/Aula4/Exercicio4.java
+++ src/Aula4/Exercicio4.java
@@ -1,18 +1,10 @@
 package Aula4;
 import javax.swing.*;
-
 public class Exercicio4 {
     public static void main(String[] args) {
-        Integer inteiro = Integer.parseInt(JOptionPane.
-                showInputDialog(null, "Digite um inteiro:", "Entrada", JOptionPane.QUESTION_MESSAGE));
-//        System.out.println(inteiro);
-        Integer inteiro2 = Integer.parseInt(JOptionPane.
-                showInputDialog(null, "Digite um inteiro:", "Entrada", JOptionPane.QUESTION_MESSAGE));
-//        System.out.println(inteiro2);
-        int soma=inteiro2+inteiro;
-        JOptionPane.
-                showMessageDialog(null,"O resultado da soma é: "+soma,"Calculo",JOptionPane.INFORMATION_MESSAGE);
-
-
+        String nome = JOptionPane.showInputDialog(null,"Digite seu nome","Entrada",JOptionPane.QUESTION_MESSAGE);
+        String sobrenome = JOptionPane.showInputDialog(null,"Digite seu nome","Entrada",JOptionPane.QUESTION_MESSAGE);
+        String nomeCompleto = nome+" "+sobrenome;
+        JOptionPane.showMessageDialog(null,nomeCompleto,"Nome Completo",JOptionPane.INFORMATION_MESSAGE);
     }
 }
