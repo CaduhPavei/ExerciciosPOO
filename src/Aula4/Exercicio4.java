3b592e7efe0dadba7d55957c6f689d6acbe983c2
RicardoPavei <127527895+CaduhPavei@users.noreply.github.com>
2023-08-22 19:03:35 -0300

    Exercício feito em sala de aula.

--- src/Aula4/Exercicio4.java
+++ src/Aula4/Exercicio4.java
@@ -3,8 +3,8 @@ import javax.swing.*;
 public class Exercicio4 {
     public static void main(String[] args) {
         String nome = JOptionPane.showInputDialog(null,"Digite seu nome","Entrada",JOptionPane.QUESTION_MESSAGE);
-        String sobrenome = JOptionPane.showInputDialog(null,"Digite seu nome","Entrada",JOptionPane.QUESTION_MESSAGE);
-        String nomeCompleto = nome+" "+sobrenome;
+        String nome2 = JOptionPane.showInputDialog(null,"Digite seu nome","Entrada",JOptionPane.QUESTION_MESSAGE);
+        String nomeCompleto = nome+" "+nome2;
         JOptionPane.showMessageDialog(null,nomeCompleto,"Nome Completo",JOptionPane.INFORMATION_MESSAGE);
     }
 }
